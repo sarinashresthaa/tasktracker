d7c3987a43714474bacb17e556430ac12ef4a909
Sarina Shrestha <sarinashrestha43@gmail.com>
2026-01-06 16:27:51 +0545

    feat: added shadcn spinner

--- src/components/todo/TodoForm.tsx
+++ src/components/todo/TodoForm.tsx
@@ -19,14 +19,16 @@ const TodoForm = () => {
   };
 
   return (
-    <form onSubmit={handleFormSubmit} className="flex gap-4">
+    <form onSubmit={handleFormSubmit} className="flex gap-4 m-8 justify-center items-center">
       <input
         type="text"
         placeholder="Add a new task..."
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
-        className=" border p-2 rounded"
+        className=" border p-2 rounded "
       />
+      <h1>Due Date:</h1>
+      <input type="date" name="" id="" className="border p-2 rounded" />
 
       <button
         type="submit"
